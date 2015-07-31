1c0a082526efa88aea392a0a08abfaf149229058
Eric Johnson <ej2586@gmail.com>
2015-07-31 11:18:57 -0700

    updated comments on functions

--- script.js
+++ script.js
@@ -1,50 +1,84 @@
 /**
  * Define all global variables here
  */
+/**
+ * student_array - global array to hold student objects
+ * @type {Array}
+ */
 var student_array = [];
+/**
+ * inputIds - id's of the elements that are used to add students
+ * @type {string[]}
+ */
+var inputIds = ['studentName', 'course', 'studentGrade'];
+
+/**
+ * addClicked - Event Handler when user clicks the add button
+ */
+function addClicked() {
+    addStudent();
+}
+
+/**
+ * cancelClicked - Event Handler when user clicks the cancel button, should clear out student form
+ */
+function cancelClicked() {
+    clearAddStudentForm();
+}
 
 /**
  * addStudent - creates a student objects based on input fields in the form and adds the object to global student array
  *
  * @return undefined
  */
-function addStudent(){
+function addStudent() {
     //create new empty object
     var obj = {};
-    var inputIds = ['studentName', 'course', 'studentGrade'];
 
     //retrieve values from each input field
-    for(var i=0; i<inputIds.length; i++){
+    for (var i = 0; i < inputIds.length; i++) {
         var id = inputIds[i];
-        var input = $("#"+id);
+        var input = $("#" + id);
         var val = input.val();
         obj[id] = val;
-
-        input.val("");
     }
 
     //add newly created object to the global student array
     student_array.push(obj);
+
+    clearAddStudentForm();
 
     //call function to update the visual data
     updateData();
 }
 
 /**
+ * clearAddStudentForm - clears out the form values based on inputIds variable
+ */
+function clearAddStudentForm() {
+    //loop through inputIds and empty the values
+    for (var i = 0; i < inputIds.length; i++) {
+        var id = inputIds[i];
+        var input = $("#" + id);
+        input.val("");
+    }
+}
+
+/**
  * calculateAverage - loop through the global student array and calculate average grade and return that value
  * @returns {number}
  */
-function calculateAverage(){
+function calculateAverage() {
     var avg = 0;
     var total = 0;
 
     //loop through student array to get the student grades
-    for(var i=0; i<student_array.length; i++){
+    for (var i = 0; i < student_array.length; i++) {
         total += parseFloat(student_array[i]["studentGrade"]);
     }
 
     //divide the total student grades by the length of the student array
-    avg = Math.round(total/student_array.length);
+    avg = Math.round(total / student_array.length);
 
     return avg;
 }
@@ -52,7 +86,7 @@ function calculateAverage(){
 /**
  * updateData - centralized function to update the average and call student list update
  */
-function updateData(){
+function updateData() {
 
     //calculate value and update the DOM
     var average = calculateAverage();
@@ -65,19 +99,19 @@ function updateData(){
 /**
  * updateStudentList - loops through global student array and appends each objects data into the student-list-container > list-body
  */
-function updateStudentList(){
+function updateStudentList() {
 
     //empty out the current student list
     $('.student-list tbody').html('');
 
     //when the student array is empty, update the student table with a message of User Info Unavailable
-    if(student_array.length == 0){
+    if (student_array.length == 0) {
         var userUnavailableRow = $('<td>').attr("colspan", 5).append($('<h3>').html("User Info Unavailable"));
         $('.student-list tbody').html(userUnavailableRow);
     }
 
-    //loop throufh student list and call addStudentToDom with each student object in the array
-    for(var i=0; i<student_array.length; i++){
+    //loop through student list and call addStudentToDom with each student object in the array
+    for (var i = 0; i < student_array.length; i++) {
         var studentObj = student_array[i];
         addStudentToDom(studentObj);
     }
@@ -88,18 +122,23 @@ function updateStudentList(){
  * into the .student_list tbody
  * @param studentObj
  */
-function addStudentToDom(studentObj){
+function addStudentToDom(studentObj) {
 
     //create new row and all columns within the row with the studentObj values
     var newTableRow = $('<tr>');
-    var nameColumn = $('<td>').html(studentObj['studentName']);
-    var courseColumn = $('<td>').html(studentObj['course']);
-    var gradeColumn = $('<td>').html(studentObj['studentGrade']);
+    //doesn't have to be a loop but I like loops :)
+    for(var index in studentObj){
+        var val = studentObj[index];
+        var newCol = $('<td>').html(val);
+        newTableRow.append(newCol);
+    }
+
+    //create the operations column and action buttons inside
     var deleteBtn = $('<button>').html("Delete").addClass("btn btn-danger");
     var operationsColumn = $('<td>').html(deleteBtn);
 
-    //append all newly created elements to the new table row
-    newTableRow.append(nameColumn, courseColumn, gradeColumn, operationsColumn);
+    //append operations column to the row
+    newTableRow.append(operationsColumn);
 
     //append the new row to the .student-list body
     $('.student-list tbody').append(newTableRow);
@@ -108,19 +147,22 @@ function addStudentToDom(studentObj){
 /**
  * reset - resets the application to initial state. Global variables reset, DOM get reset to initial load state
  */
-function reset(){
+function reset() {
 
     //empty student array
     student_array = [];
 
     //update the data now that student array is empty
     updateData();
+
+    //clear addStudent Form
+    clearAddStudentForm();
 }
 
 
 /**
  * Listen for the document to load and reset the data to the initial state
  */
-$(document).ready(function(){
+$(document).ready(function () {
     reset();
 });

048084aa4df8b71e729317683cb0a87e7aef33a6
Daniel <daniel.paschal@learningfuze.com>
2017-10-01 17:15:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,113 @@
+/* information about jsdocs: 
+* param: http://usejsdoc.org/tags-param.html#examples
+* returns: http://usejsdoc.org/tags-returns.html
+* 
 /**
- * Define all global variables here
+ * Listen for the document to load and initialize the application
  */
+$(document).ready();
+
 /**
+ * Define all global variables here.  
+ */
+/***********************
  * student_array - global array to hold student objects
  * @type {Array}
+ * example of student_array after input: 
+ * student_array = [
+ *  { name: 'Jake', course: 'Math', grade: 85 },
+ *  { name: 'Jill', course: 'Comp Sci', grade: 85 }
+ * ];
  */
 
-/**
- * inputIds - id's of the elements that are used to add students
- * @type {string[]}
- */
+/***************************************************************************************************
+* initializeApp 
+* @params {undefined} none
+* @returns: {undefined} none
+* initializes the application, including adding click handlers and pulling in any data from the server, in later versions
+*/
+function initializeApp(){
+}
 
-/**
- * addClicked - Event Handler when user clicks the add button
- */
+/***************************************************************************************************
+* addClickHandlerstoElements
+* @params {undefined} 
+* @returns  {undefined}
+*     
+*/
+function addClickHandlersToElements(){
+}
 
-/**
- * cancelClicked - Event Handler when user clicks the cancel button, should clear out student form
+/***************************************************************************************************
+ * handleAddClicked - Event Handler when user clicks the add button
+ * @param {object} event  The event object from the click
+ * @return: 
+       none
  */
-
-/**
+function handleAddClicked(){
+}
+/***************************************************************************************************
+ * handleCancelClicked - Event Handler when user clicks the cancel button, should clear out student form
+ * @param: {undefined} none
+ * @returns: {undefined} none
+ * @calls: clearAddStudentFormInputs
+ */
+function handleCancelClick(){
+}
+/***************************************************************************************************
  * addStudent - creates a student objects based on input fields in the form and adds the object to global student array
- *
+ * @param {undefined} none
  * @return undefined
+ * @calls clearAddStudentFormInputs, updateStudentList
  */
-
-/**
+function addStudent(){
+}
+/***************************************************************************************************
  * clearAddStudentForm - clears out the form values based on inputIds variable
  */
+function clearAddStudentFormInputs(){
+}
+/***************************************************************************************************
+ * renderStudentOnDom - take in a student object, create html elements from the values and then append the elements
+ * into the .student_list tbody
+ * @param {object} studentObj a single student object with course, name, and grade inside
+ */
+function renderStudentOnDom(){
+}
 
-/**
- * calculateAverage - loop through the global student array and calculate average grade and return that value
+/***************************************************************************************************
+ * updateStudentList - centralized function to update the average and call student list update
+ * @param students {array} the array of student objects
+ * @returns {undefined} none
+ * @calls renderStudentOnDom, calculateGradeAverage, renderGradeAverage
+ */
+function updateStudentList(){
+  
+}
+/***************************************************************************************************
+ * calculateGradeAverage - loop through the global student array and calculate average grade and return that value
+ * @param: {array} students  the array of student objects
  * @returns {number}
  */
-
-/**
- * updateData - centralized function to update the average and call student list update
+function calculateGradeAverage(){
+}
+/***************************************************************************************************
+ * renderGradeAverage - updates the on-page grade average
+ * @param: {number} average    the grade average
+ * @returns {undefined} none
  */
-
-/**
+function renderGradeAverage(){
+}
+/***************************************************************************************************
+* @params {array} students   the array of students
+* @returns {number} the average of all the students
+function calculateGradeAverage(){
+}
+/***************************************************************************************************
  * updateStudentList - loops through global student array and appends each objects data into the student-list-container > list-body
  */
-
-/**
- * addStudentToDom - take in a student object, create html elements from the values and then append the elements
- * into the .student_list tbody
- * @param studentObj
- */
-
-/**
- * reset - resets the application to initial state. Global variables reset, DOM get reset to initial load state
- */
+function updateStudentList(){
+}
 
 
-/**
- * Listen for the document to load and reset the data to the initial state
- */
+

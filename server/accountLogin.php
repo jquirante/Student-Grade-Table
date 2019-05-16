40a244a6754a0effa79bad4f52793f0a10e7e224
Justen Quirante <jcquirante@gmail.com>
2019-05-15 21:55:39 -0700

    Sign in debug in progress

--- server/accountLogin.php
+++ server/accountLogin.php
@@ -11,18 +11,23 @@ if ( ! empty($_POST) ) {
 
         $email = $_POST["loginEmail"];
         $password = $_POST["loginPass"];
-        $stmt = $creds->prepare("SELECT * FROM `studentGradeTableAccounts` WHERE email = ?");
+        $stmt = $creds->prepare("SELECT email FROM `studentGradeTableAccounts` WHERE email = ?");
+        // $query = "SELECT * FROM `studentGradeTableAccounts` WHERE email = $email";
+        // $result = $creds->query($query);
         $stmt->bind_param("s", $email);
         
         $stmt->execute();
         
         $result = $stmt->get_result();
-        $user = $result->fetch_object();
+        $output = $result;
+        // $user = $result->fetch_object();
         // $result = $stmt->get_result();
         // $user = $result->fetch_object();
         // $output = $user->password;
-
-        $output = $result;
+    
+    //     while ($row = $result->fetch_assoc()) {
+    //         echo $row;
+    //    }
         // $inputPass = $_POST["loginPass"];
         // $dbPass = $user->password;
 

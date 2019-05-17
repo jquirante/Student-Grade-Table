1c10e2e79f82ba817c1e26196ad396e677dc9785
Justen Quirante <jcquirante@gmail.com>
2019-05-16 22:48:05 -0700

    Sign in In progress

--- server/accountLogin.php
+++ server/accountLogin.php
@@ -6,46 +6,56 @@ require_once('mysql_creds.php');
 
 $output = null;
 
-if ( ! empty($_POST) ) {
-    if ( isset($_POST["loginEmail"]) && isset($_POST["loginPass"])) {
 
         $email = $_POST["loginEmail"];
         $password = $_POST["loginPass"];
-        $stmt = $creds->prepare("SELECT email FROM `studentGradeTableAccounts` WHERE email = ?");
-        // $query = "SELECT * FROM `studentGradeTableAccounts` WHERE email = $email";
-        // $result = $creds->query($query);
+
+        $stmt = $creds->prepare("SELECT email FROM studentGradeTableAccounts WHERE email = ?");
         $stmt->bind_param("s", $email);
-        
         $stmt->execute();
-        
         $result = $stmt->get_result();
-        $output = $result;
-        // $user = $result->fetch_object();
-        // $result = $stmt->get_result();
-        // $user = $result->fetch_object();
-        // $output = $user->password;
+        $output = mysqli_num_rows($result);
+// if ( ! empty($_POST) ) {
+//     if ( isset($_POST["loginEmail"]) && isset($_POST["loginPass"])) {
+
+//         $email = $_POST["loginEmail"];
+//         $password = $_POST["loginPass"];
+//         // $stmt = $creds->prepare("SELECT email FROM studentGradeTableAccounts WHERE email = ?");
+//         $query = "SELECT `email`,`password` FROM `studentGradeTableAccounts` WHERE email = ?";
+//         $stmt = mysqli_prepare($creds, $query);
+//         // $result = $creds->query($query);
+//         // $stmt->bind_param("s", $email);
+//         mysqli_stmt_bind_param($stmt, "s", $email);
+//         $stmt->execute();
+        
+//         $result = $stmt->get_result();
+//         $output = mysqli_num_rows($result);
+//         // $user = $result->fetch_object();
+//         // $result = $stmt->get_result();
+//         // $user = $result->fetch_object();
+//         // $output = $user->password;
     
-    //     while ($row = $result->fetch_assoc()) {
-    //         echo $row;
-    //    }
-        // $inputPass = $_POST["loginPass"];
-        // $dbPass = $user->password;
+//     //     while ($row = $result->fetch_assoc()) {
+//     //         echo $row;
+//     //    }
+//         // $inputPass = $_POST["loginPass"];
+//         // $dbPass = $user->password;
 
-        // $test = password_verify( $_POST["loginPass"], $user->password);
-        // $test2 = $inputPass == $dbPass;
+//         // $test = password_verify( $_POST["loginPass"], $user->password);
+//         // $test2 = $inputPass == $dbPass;
 
-        // $output = [
-        //     "inputPass"=>$inputPass,
-        //     "dbPass"=>$dbPass,
-        //     "test"=>$test,
-        //     "test2"=>$test2
-        // ];
-        // if ( password_verify( $_POST["loginPass"], $user->password) ) {
-        //     $_SESSION['user_id'] = $user->ID;
-        //     $output = true;
-    	// }
-    }
-}
+//         // $output = [
+//         //     "inputPass"=>$inputPass,
+//         //     "dbPass"=>$dbPass,
+//         //     "test"=>$test,
+//         //     "test2"=>$test2
+//         // ];
+//         // if ( password_verify( $_POST["loginPass"], $user->password) ) {
+//         //     $_SESSION['user_id'] = $user->ID;
+//         //     $output = true;
+//     	// }
+//     }
+// }
 
 $json_output = json_encode($output);
 

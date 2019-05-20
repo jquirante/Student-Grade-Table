14d4245656265ec5962a33c4d8aa12564f28472d
Justen Quirante <jcquirante@gmail.com>
2019-05-19 23:19:39 -0700

    Sign in in progress

--- server/accountLogin.php
+++ server/accountLogin.php
@@ -1,61 +1,64 @@
 <?php
 
-session_start();
-
 require_once('mysql_creds.php');
 
 $output = null;
 
 
-        $email = $_POST["loginEmail"];
-        $password = $_POST["loginPass"];
+    $email = $_POST["loginEmail"];
+    $password = $_POST["loginPass"];
 
-        $stmt = $creds->prepare("SELECT email FROM studentGradeTableAccounts WHERE email = ?");
-        $stmt->bind_param("s", $email);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $output = mysqli_num_rows($result);
-// if ( ! empty($_POST) ) {
-//     if ( isset($_POST["loginEmail"]) && isset($_POST["loginPass"])) {
+    if (empty($email) || empty($password)) {
+        print("EMPTY PASSWORD OR EMAIL");
+        exit();
+    }
+    else {
+        $sql = "SELECT * FROM studentGradeTableAccounts WHERE email=?;";
+        $stmt = mysqli_stmt_init($creds);
 
-//         $email = $_POST["loginEmail"];
-//         $password = $_POST["loginPass"];
-//         // $stmt = $creds->prepare("SELECT email FROM studentGradeTableAccounts WHERE email = ?");
-//         $query = "SELECT `email`,`password` FROM `studentGradeTableAccounts` WHERE email = ?";
-//         $stmt = mysqli_prepare($creds, $query);
-//         // $result = $creds->query($query);
-//         // $stmt->bind_param("s", $email);
-//         mysqli_stmt_bind_param($stmt, "s", $email);
-//         $stmt->execute();
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+            print("SQL ERROR");
+            exit();
+
+        } else {
+
+            mysqli_stmt_bind_param($stmt, "s", $email);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+
+            if ($row = mysqli_fetch_assoc($result)) {
+
+                $hashedInputPwd = hash("sha256", $password);
+                $pwdCheck = $hashedInputPwd == $row["password"];
+
+                if ($pwdCheck == false) {
+                    print("WRONG PASSWORD");
+                    print($hashedInputPwd);
+                    print($row["password"]);
+                    exit();
+                } else if ($pwdCheck == true) {
+                    session_start();
+                    $_SESSION["userId"] = $row["ID"];
+                    $_SESSION["email"] = $row["email"];
+
+                    header("Location: ../index.php?login=success");
+                    exit();
+                }
+                else {
+                    print("WRONG PASSWORD");
+                    exit();
+                }
+            } else {
+                print("NO USER IN DB");
+                exit();
+            }
+
+
+        }
+
+    }
+
         
-//         $result = $stmt->get_result();
-//         $output = mysqli_num_rows($result);
-//         // $user = $result->fetch_object();
-//         // $result = $stmt->get_result();
-//         // $user = $result->fetch_object();
-//         // $output = $user->password;
-    
-//     //     while ($row = $result->fetch_assoc()) {
-//     //         echo $row;
-//     //    }
-//         // $inputPass = $_POST["loginPass"];
-//         // $dbPass = $user->password;
-
-//         // $test = password_verify( $_POST["loginPass"], $user->password);
-//         // $test2 = $inputPass == $dbPass;
-
-//         // $output = [
-//         //     "inputPass"=>$inputPass,
-//         //     "dbPass"=>$dbPass,
-//         //     "test"=>$test,
-//         //     "test2"=>$test2
-//         // ];
-//         // if ( password_verify( $_POST["loginPass"], $user->password) ) {
-//         //     $_SESSION['user_id'] = $user->ID;
-//         //     $output = true;
-//     	// }
-//     }
-// }
 
 $json_output = json_encode($output);
 
